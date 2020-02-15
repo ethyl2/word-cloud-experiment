61e43f7d8ec79ae0c7a3efa08454216814cd67f7
Heather Nuffer <zzzuzz@gmail.com>
2020-02-14 17:08:11 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import cheese from './images/cheeseSmall.jpg';
 
 function App() {
+  const canvas = useRef();
+  const img = React.useRef();
+
+  const handleLoad = () => {
+    const ctx = canvas.current.getContext("2d");
+    ctx.fillText('Say cheese', 50, 420);
+    console.log(img);
+    ctx.drawImage(img.current, 0, 0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Canvas Experiments</h1>
       </header>
+
+      <div>
+        <canvas ref={canvas} width={640} height={425} />
+        <img onLoad={handleLoad} ref={img} src={cheese} style={{display: 'none'}} alt='cheese'/>
+      </div>
+
+
     </div>
   );
 }
