10ad3b55dfec4a4e9914d2e743e520cd81a62254
Heather Nuffer <zzzuzz@gmail.com>
2020-02-17 20:31:09 -0700

    Added longer seed string

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ const cloud = require('./cloudMaker.js');
 
 function App() {
   const [userInput, setUserInput] = useState('');
-  const [testString, setTestString] = useState('The Greatest Love of All');
+  const [testString, setTestString] = useState("Love Will Keep Us Together You Make My Dreams Nothing Even Matters Everything Has Changed A Moment Perfect Like This At Last Forever And Ever Amen Don't Get Me Wrong Roses Are Red (My Love) Happy Together Happily Ever After Knocks Me Off My Feet Like a Star Something You Got Me The Way You Look Tonight The Very Thought Of You Crazy in Love Best Part Always \
+  and Forever (What A) Wonderful World Love Song Baby Love Love Story \
+  Part-Time Lover Little Lovin' Greatest Love of All I Just Called to Say I Love You I Love You Always Forever The Power of Love That's The Way Love Goes Justify My Love Don't Let Go (Love) This Guy's In Love With You You You've Lost That Lovin' Feelin' Pretty One If You Want to Be Happy ABC This is How We Do It Jungle Boogie Signed Sealed Delivered I'm Yours Ob-La-Di Ob-La-Da Got To Give It Up Paper Planes Gimme Little Sign Upside Down Don't Stop 'Til You Get Enough September Respect Me Myself and I Bust a Move Groove Is in the Heart Holiday Please mr. Postman The Locomotion Tears of a Clown Ain't Too Proud to Beg Dancing in the Streets Thinking Out Loud Marry You All of Me Uptown Funk I Wanna Dance with Somebody Don't Stop Believin' Crazy in Love A Thousand Years I'm Yours Hey Ya");
   const [words, setWords] = useState([]);
 
   const canvas = useRef();
   const img = useRef();
-  /*
-  const [testString, setTestString] = useState("Love Will Keep Us Together You Make My Dreams Nothing Even Matters Everything Has Changed A Moment Perfect Like This At Last Forever And Ever Amen Don't Get Me Wrong Roses Are Red (My Love) Happy Together Happily Ever After Knocks Me Off My Feet Like a Star Something You Got Me The Way You Look Tonight The Very Thought Of You Crazy in Love Best Part Always \
-  and Forever (What A) Wonderful World Love Song Baby Love Love Story \
-  Part-Time Lover Little Lovin' Greatest Love of All I Just Called to Say I Love You I Love You Always Forever The Power of Love That's The Way Love Goes Justify My Love Don't Let Go (Love) This Guy's In Love With You You You've Lost That Lovin' Feelin' Pretty One If You Want to Be Happy ABC This is How We Do It Jungle Boogie Signed Sealed Delivered I'm Yours Ob-La-Di Ob-La-Da Got To Give It Up Paper Planes Gimme Little Sign Upside Down Don't Stop 'Til You Get Enough September Respect Me Myself and I Bust a Move Groove Is in the Heart Holiday Please mr. Postman The Locomotion Tears of a Clown Ain't Too Proud to Beg Dancing in the Streets Thinking Out Loud Marry You All of Me Uptown Funk I Wanna Dance with Somebody Don't Stop Believin' Crazy in Love A Thousand Years I'm Yours Hey Ya")
-  */
+  
 
   useEffect(() => {
     const testArray = testString.split(' ');
@@ -32,9 +30,9 @@ function App() {
     });
     //console.log(testMap);
     const testMapToArray = Object.entries(testMap);
-    const sortedTestMapToArray = testMapToArray.sort((a, b) => (a[1] > b[1] ? -1 : 1));
+    //const sortedTestMapToArray = testMapToArray.sort((a, b) => (a[1] > b[1] ? -1 : 1));
     //console.log(sortedTestMapToArray);
-
+    const sortedTestMapToArray = testMapToArray;
     setWords(sortedTestMapToArray.map(d => {
       return {
         text: d[0], 
@@ -63,21 +61,6 @@ function App() {
   }, [testString, userInput])
   
   const wordsToIgnore = ["is", "of", "a", "the", "this", "and", "or", "to", "in", "on", "with", "it", "was", "got", "i", "i'm"]
-
-  
-  /*
-  const words = testArray.map(d => {
-    return {text: d, size: 10 + Math.random() * 90}
-  })
-  */
-  /*
-  const words = ["Hello", "Hello", "Hello", "Hello", "Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
-    .map(function(d) {
-      console.log(d);
-      return {text: d, size: 10 + Math.random() * 90};
-    });
-  */
-  //console.log(words);
 
   const handleLoad = () => {
     const ctx = canvas.current.getContext("2d");
@@ -114,7 +97,7 @@ function App() {
     e.preventDefault();
     setTestString(testString + " " + userInput);
     setUserInput('');
-    console.log(testString);
+    //console.log(testString);
   }
 
   return (
